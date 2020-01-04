37772fae1c198493d87180b7161ba7ac700aea56
cgpu <38183826+cgpu@users.noreply.github.com>
2020-01-04 01:17:27 +0000

    Harmonises style across plots; Adds rounded barplot (#9)
    * Rearranges scale_color_gradient2() before geom_text()
    
    * Adds scale_size_continuous() to control geom_point size
    
    * Replaces hard-coded hex with geom_text(aes(colour = birth.rate)
    
    * Adds geom_text for {gr,pl}
    
    * Changes panel.background from gg-grey->white
    
    * Customises actos~starring roles barplot

--- exercises/solutions/actors.R
+++ exercises/solutions/actors.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(dplyr)
+library(ggchicklet)
 
 movies <- read.csv("exercises/data/movie_metadata.csv")
 
@@ -7,26 +8,50 @@ movies_actors <- movies%>%
     group_by(aktor=actor_1_name)%>%
     summarise(ilosc=n())%>%
     arrange(desc(ilosc))%>%
-    head(6)
+    head(15)
+
+#ggplt <- 
+            ggplot(movies_actors,aes(x     =  reorder(aktor,ilosc), 
+                                     y     =  ilosc,
+                                     fill  =  ilosc)) +
+    
+            geom_chicklet(size   = 0.30,
+                          width   = 0.85,
+                          radius = unit(0.30, "cm"))  +
+
+            theme(text              = element_text(           color = "#4A637B", face = "bold", family = 'Helvetica')
+                  ,plot.caption     = element_text(size =  9, color = "#8d99ae", face = "plain"                     ) 
+                  ,plot.title       = element_text(size = 18, color = "#2b2d42", face = "bold", hjust=0.15          )
+                  ,axis.text.y      = element_text(size = 10, color = "#8d99ae", face = "bold", hjust=1.1           )
+                  ,axis.title.x     = element_text(size = 14 ,                                  hjust = 0.15        )
+                  ,axis.text.x      = element_blank()
+                  ,axis.title.y     = element_blank()
+                  ,axis.ticks.x     = element_blank()
+                  ,axis.ticks.y     = element_blank()
+                  ,plot.margin      = unit(c(1,1,1,1),"cm")
+                  ,panel.background = element_blank()
+                  ,legend.position  = "none"
+                  ,aspect.ratio     = 0.75) + 
+
+            scale_fill_gradient2(midpoint  = mean(movies_actors$ilosc) - 2,
+                                  low      = "#4974a5",
+                                  mid      = "#4A637B",
+                                  high     = "#f35f71",
+                                  space    = "Lab" ) +
+                
+            scale_size_continuous(range = c(3,5)) +
+            
+            coord_flip() +
+                
+            geom_text(aes(label = ilosc, 
+                          size  = ilosc),
+                      color     = "white",
+                      nudge_y   = -2.5,
+                      family    = 'Helvetica') +
+            
+            labs(title   = " Actors with most starring roles\n",
+                 caption = "\nsource: Spotkania Entuzjastów R-Warsaw RUG Meetup",
+                     y   = "\nNumber of starring roles")
 
 
-ggplot(movies_actors,aes(aktor,ilosc))+
-    geom_col(width = 0.8,fill="#f35f71")+
-    geom_text(aes(label=ilosc),nudge_y = -3, color="white",size=6)+
-    labs(title="starring roles",y="Number of starring roles")+
-    coord_flip()+
-    theme(
-        
-        plot.title = element_text(face="bold",size=24,color="#8d99ae",hjust=0.2),
-        plot.margin = unit(c(1,1,1,1),"cm"),
-        
-        panel.background = element_blank(),
-        
-        axis.ticks = element_blank(),
-        axis.title.y = element_blank(),
-        axis.text.y = element_text(size=16,color="#2b2d42",face="italic",hjust=1.1),
-        axis.title.x=element_text(size=18,hjust=0.2),
-        axis.text.x=element_blank(),
-        
-        aspect.ratio = 1
-    )
+
